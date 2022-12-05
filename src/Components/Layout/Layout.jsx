e590c15276937aa1ff915c83f2183832c139e3fe
Marwa-qassem <69899679+Marwa-qassem@users.noreply.github.com>
2022-12-05 06:17:47 +0200

    Update Layout.jsx

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,19 +1,13 @@
 import React from "react";
-// import { Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import NavBar from "../Navbar/NavBar";
 import Footer from "../Footer/Footer";
-import Home from "../Home/Home";
-import Portfolio from "../Portfolio/Portfolio";
-import Contact from "../Contact/Contact";
 
 export default function Layout() {
   return (
     <>
       <NavBar />
-      {/* <Outlet></Outlet> */}
-      <Home/>
-      <Portfolio/>
-      <Contact/>
+      <Outlet></Outlet>
       <Footer />
     </>
   );
